927ae0534e5a28e6063570e3a42999945a1637eb
ahmed <01204054667apc@gmail.com>
2024-08-22 22:31:22 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -8,33 +8,35 @@ import postRoutes from "./routes/postRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import { v2 as cloudinary } from 'cloudinary'
 import { app, server } from "./socket/socket.js";
+import job from "./cron/cron.js";
 dotenv.config();
 connectDB();
+job.start();
 // const app =express();
-const PORT=process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 cloudinary.config({
-    cloud_name:process.env.CloudINARY_CLOUD_NAME,
-    api_key:process.env.CloudINARY_API_KEY,
-    api_secret:process.env.CloudINARY_API_SECRET
+	cloud_name: process.env.CloudINARY_CLOUD_NAME,
+	api_key: process.env.CloudINARY_API_KEY,
+	api_secret: process.env.CloudINARY_API_SECRET
 
 })
-app.use(express.json({limit:"80mb"}));
-app.use(express.urlencoded({extended:true}));
+app.use(express.json({ limit: "80mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/users',userRoutes)
-app.use('/api/posts',postRoutes)
-app.use('/api/messages',messageRoutes);
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/messages', messageRoutes);
 
 
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	// react app
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+// react app
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
-server.listen(PORT,()=>console.log(`server startde at http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`server startde at http://localhost:${PORT}`));
