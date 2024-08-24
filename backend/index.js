fee502c31c1e3d1391ea939ac54d51d697bb5c87
ahmed <01204054667apc@gmail.com>
2024-08-24 14:27:18 +0300

    deploy

--- backend/index.js
+++ backend/index.js
@@ -8,31 +8,24 @@ import postRoutes from "./routes/postRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import { v2 as cloudinary } from 'cloudinary'
 import { app, server } from "./socket/socket.js";
-
 dotenv.config();
 connectDB();
-const cors=require("cors")
-app.use(cors())
-var compression = require('compression')
-app.use(compression())
 // const app =express();
-const PORT = process.env.PORT || 5000;
+const PORT=process.env.PORT||5000;
 const __dirname = path.resolve();
 cloudinary.config({
-	cloud_name: process.env.CloudINARY_CLOUD_NAME,
-	api_key: process.env.CloudINARY_API_KEY,
-	api_secret: process.env.CloudINARY_API_SECRET
+    cloud_name:process.env.CloudINARY_CLOUD_NAME,
+    api_key:process.env.CloudINARY_API_KEY,
+    api_secret:process.env.CloudINARY_API_SECRET
 
 })
-app.use(express.json({ limit: "80mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:"80mb"}));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes)
-app.use('/api/posts', postRoutes)
-app.use('/api/messages', messageRoutes);
-
-
+app.use('/api/users',userRoutes)
+app.use('/api/posts',postRoutes)
+app.use('/api/messages',messageRoutes)
 
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -41,7 +34,4 @@ app.use('/api/messages', messageRoutes);
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 
-
-
-
-server.listen(PORT, () => console.log(`server startde at http://localhost:${PORT}`));
+server.listen(PORT,()=>console.log(`server startde at http://localhost:${PORT}`));
