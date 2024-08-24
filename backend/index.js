158ed8c23d3edfffd285bb88d116618883f39322
ahmed <01204054667apc@gmail.com>
2024-08-24 13:55:44 +0300

    deployment

--- backend/index.js
+++ backend/index.js
@@ -33,14 +33,14 @@ app.use('/api/posts', postRoutes)
 app.use('/api/messages', messageRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
+
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	// react app
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}
+
 
 
 
