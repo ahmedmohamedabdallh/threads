e960f21985b5e01ce7ce69882fb233cef4636d9e
ahmed <01204054667apc@gmail.com>
2024-08-26 02:52:32 +0300

    deploy

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,11 @@ import postRoutes from "./routes/postRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import { v2 as cloudinary } from 'cloudinary'
 import { app, server } from "./socket/socket.js";
+import cors from "cors"
 dotenv.config();
 connectDB();
 // const app =express();
-const port=process.env.PORT||5000;
+const PORT=process.env.PORT||5000;
 const __dirname = path.resolve();
 cloudinary.config({
     cloud_name:process.env.CloudINARY_CLOUD_NAME,
@@ -19,6 +20,7 @@ cloudinary.config({
     api_secret:process.env.CloudINARY_API_SECRET
 
 })
+app.use(cors())
 app.use(express.json({limit:"80mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -34,4 +36,4 @@ app.use('/api/messages',messageRoutes)
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 
-server.listen(port,()=>console.log(`server startde at http://localhost:${port}`));
+server.listen(PORT,()=>console.log(`server startde at http://localhost:${PORT}`));
